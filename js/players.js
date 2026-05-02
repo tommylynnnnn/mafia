134f53ea7f260df8b5b01b4bc8843717c9172ca1
tommylynnnnn <hildabaggin@gmail.com>
2026-05-02 13:49:17 -0400

    Update players.js

--- js/players.js
+++ js/players.js
@@ -1,1 +1,12 @@
+export class Player {
+  constructor(id, name, roleName, isHuman = false) {
+    this.id = id;
+    this.name = name;
+    this.roleName = roleName;
+    this.isHuman = isHuman;
+    this.alive = true;
 
+    this.blocked = false;
+    this.protected = false;
+  }
+}
